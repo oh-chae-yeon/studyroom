f3ef9f5a0bad268768fbdddd697585b241690013
oh-chae-yeon <ocy4489@naver.com>
2024-07-22 00:20:19 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project_studyroom/reservation.dart';
+import 'package:project_studyroom/confirm_reservation.dart';
+//import 'package:project_studyroom/reservation.dart';
 
 void main() {
   //애플리케이션 실행
@@ -8,16 +9,18 @@ void main() {
 
 //statelesswidget을 상속받아, 불변의 위젯 정의
 class MyApp extends StatelessWidget {
+  // ignore: use_super_parameters
   const MyApp({super.key});
 //생성자로 key를 받아 부모 클래스의 생성자에 전달
 //const는 생성자를 상수로 만듦
-
-//위젯의 UI를 정의하는 메서드 MaterialApp을 반환하여 어플의 기본설정을 제공
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ReservationScreen(),
+      home: Confirmreservation(),
     );
   }
 }
+//위젯의 UI를 정의하는 메서드 MaterialApp을 반환하여 어플의 기본설정을 제공
+
+
